d00373c66083ab6e9777541595e6cdb2de661cf2
ynonp <ynonperek@gmail.com>
2025-01-25 20:14:20 +0200

    moved to websocket driver because next14 has weird cache policy

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -6,6 +6,7 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 config({ path: ".env" }); // or .env.local
 
 const pool = new Pool({connectionString: process.env.DATABASE_URL! });
+
 export const db = drizzle(pool);
 
 
